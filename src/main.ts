d0711deffff47a33c037fadb8a94845d0ae6754b
neokoni <dulin081682@126.com>
2025-12-18 02:15:21 +0800

    style: add font

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-// import './assets/main.css'
+import './assets/main.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/noto-sans-sc/400.css';
+import '@fontsource/noto-sans-sc/500.css';
+import '@fontsource/noto-sans-sc/700.css';
+
 import 'mdui/mdui.css';
 import 'mdui';
 
