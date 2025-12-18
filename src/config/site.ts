cf61c99a743879decd7f7f98cdc3a103ea745351
neokoni <dulin081682@126.com>
2025-12-18 16:53:19 +0800

    feat: apply real icp info for site

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,14 @@
 export const siteConfig = [
   {
-  icp: "萌ICP备20246666号",
-  icpLink: "https://icp.gov.moe/?keyword=20246666"
+    icp: "萌ICP备20252300号",
+    icpLink: "https://icp.gov.moe/?keyword=20252300"
+  },
+  {
+    icp: "蜀ICP备2024074700号-2",
+    icpLink: "https://beian.miit.gov.cn/"
+  },
+  {
+    icp: "川公网安备51130402000150号",
+    icpLink: "https://beian.mps.gov.cn/#/query/webSearch?code=51130402000150"
   }
 ]
